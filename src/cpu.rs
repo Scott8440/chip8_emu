1a9e0a55ee41591082798e40832dde9147aa0bad
Scott Smith <scott8440@gmail.com>
2025-02-11 21:28:31 -0800

    emulate cycle setup

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,8 @@
+use core::panic;
+
+const PROGRAM_START: u16 = 0x200;
+const MEMORY_SIZE: usize = 4096;
+
 #[derive(Debug)]
 pub struct CPU {
     opcode: u16,
@@ -11,7 +16,7 @@ pub struct CPU {
     sound_timer: u8, // write only
 
     // Memory
-    memory: [u8; 4096],
+    memory: [u8; MEMORY_SIZE],
 
     i: u16,  // index register. Max = 0xfff
     pc: u16, // program counter. Max = 0xfff
@@ -35,13 +40,47 @@ impl CPU {
             v: [0; 16],
             delay_timer: 0,
             sound_timer: 0,
-            memory: [0; 4096],
+            memory: [0; MEMORY_SIZE],
             i: 0,
             pc: 0,
             gfx: [0; 64 * 32],
             stack: [0; 16],
             sp: 0,
             keys: [0; 16],
+        }
+    }
+
+    pub fn initialize(&mut self) {
+        self.pc = PROGRAM_START;
+        self.opcode = 0;
+        self.i = 0;
+        self.sp = 0;
+    }
+
+    pub fn load(&mut self, input: Vec<u8>) {
+        let size = input.len();
+        if (PROGRAM_START + size as u16) as usize > MEMORY_SIZE {
+            panic!("Program is too large to load.");
+        }
+        for (i, &byte) in input.iter().enumerate() {
+            let idx: usize = (PROGRAM_START + i as u16) as usize;
+            self.memory[idx] = byte;
+        }
+    }
+
+    pub fn cycle(&mut self) {
+        // Fetch opcode
+        self.opcode = (self.memory[self.pc as usize] as u16) << 8
+            | (self.memory[self.pc as usize + 1] as u16);
+
+        match self.opcode & 0xF000 {
+            0xA000 => {
+                // ANNN: Sets I to address NNN
+                println!("ANNN")
+            }
+            _ => {
+                println!("Unknown opcode: 0x{:x}", self.opcode);
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 mod cpu;
 
+const SAMPLE_MEMORY: [u8; 4096] = [0; 4096];
+
 fn main() {
-    let cpu = cpu::CPU::new();
+    let mut test_memory: Vec<u8> = Vec::new();
+    test_memory.push(0xA2);
+    test_memory.push(0xF0);
+
+    let mut cpu = cpu::CPU::new();
+    cpu.initialize();
+    cpu.load(test_memory);
+    cpu.cycle();
 }
