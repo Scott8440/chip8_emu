a13fccab28c7bab71f24783411ac575ed83ceed3
Scott Smith <scott8440@gmail.com>
2025-02-11 22:44:11 -0800

    first few opcode implementations

--- src/cpu.rs
+++ src/cpu.rs
@@ -55,6 +55,7 @@ impl CPU {
         self.opcode = 0;
         self.i = 0;
         self.sp = 0;
+        self.v = [0; 16];
     }
 
     pub fn load(&mut self, input: Vec<u8>) {
@@ -74,9 +75,11 @@ impl CPU {
             | (self.memory[self.pc as usize + 1] as u16);
 
         match self.opcode & 0xF000 {
-            0x000 => match self.opcode & 0x00FF {
+            0x0 => match self.opcode & 0x00FF {
                 0xE0 => {
-                    println!("clear");
+                    // Clear the screen
+                    self.gfx = [0; 64 * 32];
+                    self.pc += 2;
                 }
                 0xEE => {
                     println!("return");
@@ -86,44 +89,96 @@ impl CPU {
                 }
             },
             0x1000 => {
-                println!("1NNN")
+                // Jump to address NNN
+                println!("1NNN");
+                self.pc = self.opcode & 0x0FFF;
             }
             0x2000 => {
-                println!("2NNN")
+                // Call subroutine at NNN
+                println!("2NNN");
+                self.stack[self.sp as usize] = self.pc;
+                self.sp += 1;
+                self.pc = self.opcode & 0x0FFF;
             }
             0x3000 => {
-                println!("3XNN")
+                // Skip next instruction if vX == NN
+                println!("3XNN");
+                self.pc += 2;
+                if self.v[(self.opcode & 0x0F00) as usize >> 8] == (self.opcode & 0x00FF) as u8 {
+                    self.pc += 2;
+                }
             }
             0x4000 => {
-                println!("4XNN")
+                // Skip next instruction if vX != NN
+                println!("4XNN");
+                self.pc += 2;
+                if self.v[(self.opcode & 0x0F00) as usize >> 8] != (self.opcode & 0x00FF) as u8 {
+                    self.pc += 2;
+                }
             }
             0x5000 => {
-                println!("5XY0")
+                // Skip next instruction if VX == VY
+                println!("5XY0");
+                self.pc += 2;
+                if self.v[(self.opcode & 0x0F00) as usize >> 8]
+                    == self.v[(self.opcode & 0x00F0) as usize >> 4]
+                {
+                    self.pc += 2;
+                }
             }
             0x6000 => {
-                println!("6XNN")
+                // Store NN in vX
+                println!("6XNN");
+                self.v[(self.opcode & 0x0F00) as usize >> 8] = (self.opcode & 0x00FF) as u8;
             }
             0x7000 => {
-                println!("7XNN")
+                println!("7XNN");
+                self.v[(self.opcode & 0x0F00) as usize >> 8] += (self.opcode & 0x00FF) as u8;
             }
             0x8000 => match self.opcode & 0xF {
                 0x0 => {
-                    println!("8XY0")
+                    // Store VY in VX
+                    println!("8XY0");
+                    self.v[(self.opcode & 0x0F00) as usize >> 8] =
+                        self.v[(self.opcode & 0x00F0) as usize >> 4];
                 }
                 0x1 => {
-                    println!("8XY1")
+                    // store VY | VX in VX
+                    println!("8XY1");
+                    self.v[(self.opcode & 0x0F00) as usize >> 8] |=
+                        self.v[(self.opcode & 0x00F0) as usize >> 4];
                 }
                 0x2 => {
-                    println!("8XY2")
+                    // store VY & VX in VX
+                    println!("8XY2");
+                    self.v[(self.opcode & 0x0F00) as usize >> 8] &=
+                        self.v[(self.opcode & 0x00F0) as usize >> 4];
                 }
                 0x3 => {
-                    println!("8XY3")
+                    // store VY xor VX in VX
+                    println!("8XY3");
+                    self.v[(self.opcode & 0x0F00) as usize >> 8] ^=
+                        self.v[(self.opcode & 0x00F0) as usize >> 4];
                 }
                 0x4 => {
-                    println!("8XY4")
+                    // Add VY to VX
+                    // if carry occurred, set vf to 01 else vf to 00
+                    println!("8XY4");
+                    let x = (self.opcode & 0x0F00) as usize >> 8;
+                    let y = (self.opcode & 0x00F0) as usize >> 4;
+                    let (result, overflow) = self.v[x].overflowing_add(self.v[y]);
+                    self.v[x] = result;
+                    self.v[0xF] = if overflow { 0x01 } else { 0x00 };
                 }
                 0x5 => {
-                    println!("8XY5")
+                    println!("8XY5");
+                    // Sub VY from VX
+                    // if borrow occurred, set vf to 00 else vf to 01
+                    let x = (self.opcode & 0x0F00) as usize >> 8;
+                    let y = (self.opcode & 0x00F0) as usize >> 4;
+                    let (result, borrow) = self.v[x].overflowing_sub(self.v[y]);
+                    self.v[x] = result;
+                    self.v[0xF] = if borrow { 0x00 } else { 0x01 };
                 }
                 0x6 => {
                     println!("8XY6")
@@ -207,6 +262,8 @@ impl CPU {
 
 #[cfg(test)]
 mod tests {
+    use std::arch::x86_64::_MM_FROUND_NO_EXC;
+
     use super::*;
     use pretty_assertions::assert_eq;
 
@@ -230,7 +287,7 @@ mod tests {
         let mut cpu = setup();
         let program = vec![0xA2, 0xB4]; // ANNN instruction
         cpu.load(program.clone());
-        
+
         // Verify program was loaded at correct address
         assert_eq!(cpu.memory[PROGRAM_START as usize], program[0]);
         assert_eq!(cpu.memory[PROGRAM_START as usize + 1], program[1]);
@@ -239,17 +296,114 @@ mod tests {
     #[test]
     fn test_clear_screen() {
         let mut cpu = setup();
-        // Load CLS instruction (0x00E0)
-        cpu.load(vec![0x00, 0xE0]);
-        
-        // Set some pixels to 1
+        let program = vec![0x00, 0xE0];
+        cpu.load(program.clone());
         cpu.gfx[0] = 1;
-        cpu.gfx[100] = 1;
-        
+        cpu.gfx[64] = 1;
         cpu.cycle();
-        
-        // Verify all pixels are cleared
-        assert!(cpu.gfx.iter().all(|&pixel| pixel == 0));
-        assert_eq!(cpu.pc, PROGRAM_START + 2);
+        assert!(cpu.gfx.iter().all(|&x| x == 0));
+        assert!(cpu.pc == PROGRAM_START + 2);
+    }
+
+    #[test]
+    fn test_jump() {
+        let mut cpu = setup();
+        let program = vec![0x12, 0x34];
+        cpu.load(program.clone());
+        cpu.cycle();
+        assert!(cpu.pc == 0x1234 & 0x0FFF);
+    }
+
+    #[test]
+    fn test_call_subroutine() {
+        let mut cpu = setup();
+        let program = vec![0x22, 0x34];
+        cpu.load(program.clone());
+        cpu.cycle();
+        assert!(cpu.sp == 1);
+        assert!(cpu.stack[0] == 0x200);
+        assert!(cpu.pc == 0x1234 & 0x0FFF);
+    }
+
+    #[test]
+    fn test_skip_equal() {
+        let mut cpu = setup();
+        let program = vec![0x30, 0x11];
+
+        cpu.load(program.clone());
+        cpu.v[0] = 0x11;
+        cpu.cycle();
+        assert!(cpu.pc == 0x204);
+
+        cpu.initialize();
+        cpu.load(program.clone());
+        cpu.cycle();
+        assert!(cpu.pc == 0x202);
+    }
+
+    #[test]
+    fn test_skip_not_equal() {
+        let mut cpu = setup();
+        let program = vec![0x40, 0x11];
+
+        cpu.load(program.clone());
+        cpu.v[0] = 0x11;
+        cpu.cycle();
+        assert!(cpu.pc == 0x202);
+
+        cpu.initialize();
+        cpu.load(program.clone());
+        cpu.cycle();
+        assert!(cpu.pc == 0x204);
+    }
+
+    #[test]
+    fn test_skip_equal_registers() {
+        let mut cpu = setup();
+        let program = vec![0x51, 0x20];
+        cpu.load(program.clone());
+        cpu.v[1] = 0x01;
+        cpu.v[2] = 0x00;
+
+        cpu.cycle();
+        assert!(cpu.pc == 0x202);
+
+        cpu.initialize();
+        cpu.load(program.clone());
+        cpu.v[1] = 0x01;
+        cpu.v[2] = 0x01;
+        cpu.cycle();
+        assert!(cpu.pc == 0x204);
+    }
+
+    #[test]
+    fn test_set_vx() {
+        let mut cpu = setup();
+        let program = vec![0x60, 0x12];
+        cpu.load(program.clone());
+        cpu.cycle();
+        assert!(cpu.v[0] == 0x12);
+    }
+
+    #[test]
+    fn test_add_vx() {
+        let mut cpu = setup();
+        let program = vec![0x70, 0x12, 0x70, 0x12];
+        cpu.load(program.clone());
+        cpu.cycle();
+        assert!(cpu.v[0] == 0x12);
+        cpu.cycle();
+        assert!(cpu.v[0] == 0x12 + 0x12);
+    }
+
+    #[test]
+    fn test_set_vx_vy() {
+        let mut cpu = setup();
+        let program = vec![0x80, 0x10];
+        cpu.load(program.clone());
+        cpu.v[0] = 0x10;
+        cpu.v[1] = 0x20;
+        cpu.cycle();
+        assert!(cpu.v[0] == 0x20);
     }
 }
