17edfeb110637a8a8eee39de40b849ad30ddaa16
Scott8440 <scott8440@gmail.com>
2025-02-13 22:58:15 -0800

    feat: Add basic window rendering with a single white pixel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
 mod cpu;
 mod fontset;
 
+use minifb::{Key, Window, WindowOptions};
+
+fn get_window() -> Window {
+    let width = 100;
+    let height = 100;
+
+    Window::new(
+        "Test",
+        width,
+        height,
+        WindowOptions {
+            resize: true,
+            scale: minifb::Scale::X8,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap()
+}
+
 fn main() {
+    let mut window = get_window();
+    let x = 50;
+    let y = 50;
+    let mut buffer: Vec<u32> = vec![0; 100 * 100];
+    buffer[y * 100 + x] = 0xFFFFFFFF; // white pixel
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        window.update_with_buffer(&buffer, 100, 100).unwrap();
+    }
+
     let mut test_memory: Vec<u8> = Vec::new();
     test_memory.push(0xA2);
     test_memory.push(0xF0);
