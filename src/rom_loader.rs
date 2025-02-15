60eedf62129abc4c7654650df48392e9c8f68fdc
Scott8440 <scott8440@gmail.com>
2025-02-15 01:35:34 -0800

    remove unused import

--- src/rom_loader.rs
+++ src/rom_loader.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::Read;
 use std::path::Path;
 
 pub trait RomLoader {
@@ -67,5 +67,4 @@ mod tests {
 
         assert_eq!(expected_program, read_program);
     }
-
 }
